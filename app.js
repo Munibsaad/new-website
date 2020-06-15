0cdcb7903ea60d0d0227f065f3aca1a410601879
abcweb <munibsaad12@gmail.com>
2020-06-15 22:34:23 +0500

    add java file

---
+++ app.js
@@ -0,0 +1,66 @@
+// alert("hello world")
+
+
+
+// alert("Error Please Enter valid password")
+
+// alert("Welcome to js land ")
+
+
+
+// alert("hello world<br/> hello world")
+
+// alert("Welocme to js")
+
+// alert("hello...i can run this js through browser")
+
+// var username =prompt("enter your first name")
+// var secondname =prompt("enter yous second name")
+// alert(username + secondname)
+
+// var age = prompt("enter your age");
+
+// var clas = prompt("enter your clas");
+
+// var email =prompt("enter your email");
+
+
+
+// alert(age)
+// alert(clas);
+
+// alert(email);
+
+// let arr=["cake","bisket","pastery"]
+
+// arr.indexOf("cake")
+
+// var item=["pencil","eraser","cookie"]
+
+// var search = prompt(" enter search value")
+
+// if(item.indexOf(search) !== -1){
+//     alert("yes" + search+ "this is available")
+// }else{
+//     alert("no" + "this is not available")
+// }
+
+
+
+
+
+
+// var arr=["nokia","samsung","apple","huawei"]
+// document.write("<select>")
+// for(var i = 0; i < arr.length; i++){
+//     document.write("<option>" +  arr[i] + "</option>")
+// }
+// document.write("</select>")
+
+
+
+
+
+
+
+
